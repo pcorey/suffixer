8f050a6486a34bed76f5b1b679d819a392af31d8
Pete Corey <petecorey@gmail.com>
2014-12-17 19:26:56 -0800

    Added dictionary lookup

---
+++ client/main.js
@@ -0,0 +1,46 @@
+Session.set('suffix', 'io');
+Session.set('word', '');
+Session.set('definition', '');
+
+Template.body.helpers({
+    entries: function() {
+        return Dictionary.find({});
+    }
+});
+
+Template.body.events({
+    'keyup input[name=word]': function(e, t) {
+        console.log('word change');
+        Session.set('word', t.find('input[name=word]').value);
+    },
+    'keyup input[name=suffix]': function(e, t) {
+        Session.set('suffix', t.find('input[name=suffix]').value);
+    },
+    'keyup input[name=definition]': function(e, t) {
+        Session.set('definition', t.find('input[name=definition]').value);
+    }
+})
+
+Meteor.autorun(function() {
+    var regex = Session.get('word')
+        ? ('.*?'+Session.get('word')+'.*?'+Session.get('suffix')+'$')
+        : (Session.get('suffix')+'$');
+    var definitionRegex = Session.get('definition')
+        ? ('.*?'+Session.get('definition')+'.*?')
+        : '';
+    console.log('regex:',regex);
+    
+    Meteor.subscribe('dictionary', {
+        word: {$regex: regex, $options: 'i'},
+        definition: {$regex: definitionRegex, $options: 'i'}
+    }, {
+        limit: 20,
+        sort: {word: 1}
+    });
+});
+
+//TODO: Move to full text seach mongo instance:
+//http://www.meteorpedia.com/read/Fulltext_search
+
+//TODO: Hook up namecheap API:
+//https://www.namecheap.com/support/api/methods/domains/check.aspx
