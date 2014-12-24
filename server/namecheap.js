6affa35b57111b911b1f190d71cddf05672e2a08
Pete Corey <petecorey@gmail.com>
2014-12-23 16:06:28 -0800

    Added status update process

--- server/namecheap.js
+++ server/namecheap.js
@@ -12,8 +12,14 @@ Meteor.startup(function() {
 Meteor.publish('dictionary-namecheap', function(selector, options) {
     var results = Dictionary.find(selector, options);
     _.forEach(results.fetch(), function(result) {
-        if (result) {
-            console.log(result.word);
+        if (!result.last_checked ||
+            !moment(result.last_checked).add(1,'minute').isAfter(moment())) {
+            console.log('calling timeout for result ',result.word);
+            Dictionary.update(result._id, {$set: {last_checked: new Date()}});
+            setTimeout(Meteor.bindEnvironment(function() {
+                console.log('Updating result ',result.word);
+                Dictionary.update(result._id, {$set: {status: 'R'}});
+            }), Math.random()*2000);
         }
     });
     return results;
