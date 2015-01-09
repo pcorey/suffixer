0212ddec52340138654fd1ff974345152f62db9c
Pete Corey <petecorey@gmail.com>
2015-01-09 11:47:18 -0800

    Removed env.js

--- server/env.js
+++
@@ -1,4 +0,0 @@
-process.env.NAMECHEAP_USERNAME = process.env.NAMECHEAP_USERNAME || 'pcorey';
-process.env.NAMECHEAP_API_KEY = process.env.NAMECHEAP_API_KEY || '1bf617b1249d4d17be39eb46cfbfebb7';
-process.env.NAMECHEAP_CLIENT_IP = process.env.NAMECHEAP_CLIENT_IP || '68.111.210.205';
-process.env.NAMECHEAP_SANDBOX = process.env.NAMECHEAP_SANDBOX || true;
