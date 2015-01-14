84d4c501e4dd08e2a7feab197b8c4f29d57e9647
Pete Corey <petecorey@gmail.com>
2015-01-14 15:10:18 -0800

    bootstrap and flat-ui

--- client/controls/controls.js
+++ client/controls/controls.js
@@ -1,3 +1,9 @@
+Template.controls.rendered = function() {
+    console.log('in rendered');
+    $('#show-registered').bootstrapSwitch();
+    $('#suffix').select2();
+};
+
 Template.controls.helpers({
     tlds: function() {
         return NamecheapTLDs.find({});
